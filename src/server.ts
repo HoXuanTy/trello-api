43fdbe5253bf07b1823aa256b053b66ede7dcd45
Ho Xuan Ty <hoxuanty2003qng@gmail.com>
2024-07-09 15:24:42 +0700

    init code base

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express'
 const app: Express = express()
 
-const hostname = "localhost";
-const post = 8000;
+const hostname = 'localhost'
+const post = 8000
 
-app.get("/", function (req: Request, res: Response) {
-  res.send("Hello World");
-});
+app.get('/', function (req: Request, res: Response) {
+  res.send('Hello World')
+})
 
 app.listen(post, hostname, () => {
-  console.log(`I'm running server at http://${hostname}:${post}/`);
+  console.log(`I'm running server at http://${hostname}:${post}/`)
 })
