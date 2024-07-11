1af002612023d9cd4c3cca9792d50046f2da017d
Ho Xuan Ty <hoxuanty2003qng@gmail.com>
2024-07-11 15:33:59 +0700

    feat: add connect mongodb cloud atlas

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,1 +1,18 @@
-// file example
+import 'dotenv/config'
+
+declare var process: {         //?????
+    env: {
+        MONGODB_URI: string;
+        DATABASE_NAME: string;
+        APP_HOST: string;
+        APP_POST: number;
+    }
+}
+
+export const env = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    APP_HOST: process.env.APP_HOST,
+    APP_POST: process.env.APP_POST,
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,34 @@
+import exitHook from 'async-exit-hook'
 import express, { Express, Request, Response } from 'express'
-const app: Express = express()
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import {env} from '~/config/environment'
+const START_SERVER = () => {
+  const app: Express = express()
 
-const hostname = 'localhost'
-const post = 8000
+  app.get('/', async function (req: Request, res: Response) {
+    res.end('Hello World')
+  })
 
-app.get('/', function (req: Request, res: Response) {
-  res.send('Hello World')
-})
+  app.listen(env.APP_POST, env.APP_HOST, () => {
+    console.log(`I'm running server at http://${env.APP_HOST}:${env.APP_POST}/`)
+  })
 
-app.listen(post, hostname, () => {
-  console.log(`I'm running server at http://${hostname}:${post}/`)
-})
+  exitHook(() =>{
+    CLOSE_DB()
+    console.log('Disconnected from MongoDb Cloud Atlas');
+  })
+}
+
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Atlas!')
+
+    //Start the server back-end after a successful database connection
+    START_SERVER()
+  } catch (error) {
+    console.error(error);
+    process.exit(0)
+  }
+})()
