c42d2e77b39da1607264d2cbba60e6a80930021c
Ho Xuan Ty <hoxuanty2003qng@gmail.com>
2024-07-15 14:50:35 +0700

    feat: add mode dev production

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config'
 
-declare var process: {         //?????
+declare var process: {
     env: {
-        MONGODB_URI: string;
-        DATABASE_NAME: string;
-        APP_HOST: string;
-        APP_POST: number;
+        MONGODB_URI: string
+        DATABASE_NAME: string
+        APP_HOST: string
+        APP_POST: number
+        BUILD_MODE: string
     }
 }
 
@@ -14,5 +15,7 @@ export const env = {
     DATABASE_NAME: process.env.DATABASE_NAME,
     APP_HOST: process.env.APP_HOST,
     APP_POST: process.env.APP_POST,
+
+    BUILD_MODE: process.env.BUILD_MODE
 }
 
