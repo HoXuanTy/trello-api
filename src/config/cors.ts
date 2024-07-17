60a1eb6d587b82437e2ef2e4999a17032657e9e0
Ho Xuan Ty <hoxuanty2003qng@gmail.com>
2024-07-17 16:02:47 +0700

    feat: config cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,1 +1,21 @@
-// file example
+import { WHITELIST_DOMAINS } from '~/utils/constants'
+import { env } from '~/config/environment'
+import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
+import { CorsOptions } from 'cors'
+
+export const corsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        if (!origin && env.BUILD_MODE === 'dev') {
+            return callback(null, true)
+        }
+
+        if (origin && WHITELIST_DOMAINS.includes(origin)) {
+            return callback(null, true)
+        }
+
+        return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy`))
+    },
+
+    optionsSuccessStatus: 200
+}
